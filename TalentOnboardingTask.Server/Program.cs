146a163a9cb0e7aacb15f36fe087cbd6c6da80e8
Carlo M <carlo.mabalatan@gmail.com>
2025-10-22 17:54:40 +1100

    Removed CORS, commented out swagger

--- TalentOnboardingTask.Server/Program.cs
+++ TalentOnboardingTask.Server/Program.cs
@@ -4,47 +4,25 @@ using TalentOnboardingTask.Server.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<OnboardingTaskDBContext>(option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
-// CORS Services
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-            builder => builder.WithOrigins("https://localhost:54265")
-                               .AllowAnyHeader()
-                               .AllowAnyMethod());
-
-});
-
-// CORS Services END -----------------------------------------------------
+    option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDatabase")));
 
 var app = builder.Build();
-
-// CORS
-app.UseCors("AllowSpecificOrigins");
-// CORS END -----------------------------------------------------
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-//else
+//if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 //{
-//    app.UseExceptionHandler("/error"); // Global error handling, there should be a correct middleware order
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
